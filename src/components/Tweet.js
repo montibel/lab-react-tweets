33af7b619af6ae87c8b700dfb2e68380f6513d96
montibel <113550254+montibel@users.noreply.github.com>
2022-11-14 17:12:36 +0100

    components files ready

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,11 @@
 import React from "react"
+import ProfileImage from "./ProfileImage";
+
 function Tweet({tweet}) {
   console.log({tweet})
   return (
     <div className="tweet">
-      <img
-        src={tweet.user.image}
-        className="profile"
-        alt="profile"
-      /> 
+      <ProfileImage image = {tweet.user.image}/> 
 
       <div className="body">
         <div className="top">
