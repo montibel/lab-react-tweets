eaa995bb0a907f2573542c45a4692ec9c64c7c1d
montibel <113550254+montibel@users.noreply.github.com>
2022-11-14 18:49:20 +0100

    message done

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,1 +1,13 @@
 
+import React from 'react'
+
+function Message({message}){
+    return(
+
+        <p className="message">
+           {message}
+         </p>
+    )
+}
+
+export default Message;
